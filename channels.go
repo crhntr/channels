4fd0d5e4a52f432bf3fa6e79f37591fed8bb8969
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-12-06 20:33:54 -0800

    add SendEach and CountReceived

--- channels.go
+++ channels.go
@@ -4,3 +4,22 @@ func Drain[T any](c <-chan T) {
 	for range c {
 	}
 }
+
+func SendEach[T any](in []T) <-chan T {
+	c := make(chan T)
+	go func() {
+		defer close(c)
+		for _, v := range in {
+			c <- v
+		}
+	}()
+	return c
+}
+
+func CountReceived[T any](in <-chan T) int {
+	count := 0
+	for range in {
+		count++
+	}
+	return count
+}
--- channels_test.go
+++ channels_test.go
@@ -18,3 +18,19 @@ func TestDrain_receive(t *testing.T) {
 	close(c)
 	channels.Drain(c)
 }
+
+func TestSendEach(t *testing.T) {
+	t.Run("it does not block", func(t *testing.T) {
+		channels.Drain(channels.SendEach([]int{1, 2, 3}))
+	})
+	t.Run("it handles an empty list", func(t *testing.T) {
+		channels.Drain(channels.SendEach[int](nil))
+	})
+}
+
+func TestCountReceived(t *testing.T) {
+	n := channels.CountReceived(channels.SendEach[int]([]int{1, 2, 3}))
+	if n != 3 {
+		t.Fatal()
+	}
+}
