b031bd072191e7d1a76804a92553335e70be4a25
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-01-02 13:09:15 -0800

    test ReceiveElements

--- channels.go
+++ channels.go
@@ -10,7 +10,7 @@ func Drain[T any](c <-chan T) {
 	}
 }
 
-func SendEach[T any](in []T) <-chan T {
+func SendSliceElements[T any](in []T) <-chan T {
 	c := make(chan T)
 	go func() {
 		defer close(c)
@@ -19,6 +19,14 @@ func SendEach[T any](in []T) <-chan T {
 		}
 	}()
 	return c
+}
+
+func ReceiveElements[T any](c <-chan T) []T {
+	var list []T
+	for v := range c {
+		list = append(list, v)
+	}
+	return slices.Clip(list)
 }
 
 func CountReceived[T any](in <-chan T) int {
@@ -50,4 +58,59 @@ func FanIn[T any](channels ...<-chan T) <-chan T {
 		}
 	}()
 	return c
+}
+
+func FanOut[T any](n int, in <-chan T) []<-chan T {
+	channels := make([]chan T, n)
+	for i := range channels {
+		channels[i] = make(chan T)
+	}
+	go fanOut(in, sendOnly(channels))
+	return receiveOnly(channels)
+}
+
+func fanOut[T any](in <-chan T, channels []chan<- T) {
+	defer closeAll(channels)
+	for v := range in {
+		val := reflect.ValueOf(v)
+		cases := make([]reflect.SelectCase, len(channels))
+		for i := range channels {
+			if channels[i] == nil {
+				continue
+			}
+			cases[i] = reflect.SelectCase{
+				Dir:  reflect.SelectSend,
+				Chan: reflect.ValueOf(channels[i]),
+				Send: val,
+			}
+		}
+		for len(cases) > 0 {
+			chosen, _, ok := reflect.Select(cases)
+			if !ok {
+				cases = slices.Delete(cases, chosen, chosen+1)
+			}
+		}
+	}
+}
+
+func closeAll[T any](channels []chan<- T) {
+	for _, c := range channels {
+		close(c)
+	}
+}
+
+func receiveOnly[T any](in []chan T) []<-chan T {
+	result := make([]<-chan T, len(in))
+	for i := range in {
+		result[i] = in[i]
+	}
+	return result
+}
+
+func sendOnly[T any](in []chan T) []chan<- T {
+	result := make([]chan<- T, len(in))
+	for i := range in {
+		result[i] = in[i]
+	}
+	return result
 }
--- channels_test.go
+++ channels_test.go
@@ -20,19 +20,19 @@ func TestDrain_receive(t *testing.T) {
 	channels.Drain(c)
 }
 
-func TestSendEach(t *testing.T) {
+func TestSendSliceElements(t *testing.T) {
 	t.Run("it does not block", func(t *testing.T) {
-		channels.Drain(channels.SendEach([]int{1, 2, 3}))
+		channels.Drain(channels.SendSliceElements([]int{1, 2, 3}))
 	})
 	t.Run("it handles an empty list", func(t *testing.T) {
-		channels.Drain(channels.SendEach[int](nil))
+		channels.Drain(channels.SendSliceElements[int](nil))
 	})
 }
 
 func TestCountReceived(t *testing.T) {
 	for i := 0; i < 3; i++ {
 		t.Run("it counts "+strconv.Itoa(i), func(t *testing.T) {
-			n := channels.CountReceived(channels.SendEach(make([]int, i)))
+			n := channels.CountReceived(channels.SendSliceElements(make([]int, i)))
 			if n != i {
 				t.Fail()
 			}
@@ -50,8 +50,8 @@ func TestCountReceived(t *testing.T) {
 
 func TestFanIn(t *testing.T) {
 	t.Run("2 channels", func(t *testing.T) {
-		evens := channels.SendEach([]int{2, 4, 6})
-		odds := channels.SendEach([]int{1, 3, 5})
+		evens := channels.SendSliceElements([]int{2, 4, 6})
+		odds := channels.SendSliceElements([]int{1, 3, 5})
 
 		n := channels.CountReceived(channels.FanIn(evens, odds))
 		if n != 6 {
@@ -66,7 +66,7 @@ func TestFanIn(t *testing.T) {
 	})
 
 	t.Run("2 channels first closed", func(t *testing.T) {
-		odds := channels.SendEach([]int{1, 3, 5})
+		odds := channels.SendSliceElements([]int{1, 3, 5})
 		closed := make(chan int)
 		close(closed)
 
@@ -77,7 +77,7 @@ func TestFanIn(t *testing.T) {
 	})
 
 	t.Run("2 channels second closed", func(t *testing.T) {
-		odds := channels.SendEach([]int{1, 3, 5})
+		odds := channels.SendSliceElements([]int{1, 3, 5})
 		closed := make(chan int)
 		close(closed)
 
@@ -98,4 +98,50 @@ func TestFanIn(t *testing.T) {
 			t.Fail()
 		}
 	})
+}
+
+func TestFanOut(t *testing.T) {
+	t.Run("it consistently counts", func(t *testing.T) {
+		for i := 0; i < 10; i++ {
+			zeros := channels.SendSliceElements(make([]int, 10))
+			n := channels.CountReceived(channels.FanIn(channels.FanOut(5, zeros)...))
+			if exp := 50; n != exp {
+				t.Error("got: ", n, " exp: ", exp)
+			}
+		}
+	})
+	t.Run("it send the same value to all channels", func(t *testing.T) {
+		in := make([]int, 100)
+		for i := range in {
+			in[i] = i
+		}
+		zeros := channels.SendSliceElements(in)
+		const numberOfChannels = 2
+		out := channels.ReceiveElements(channels.FanIn(channels.FanOut(numberOfChannels, zeros)...))
+		for _, v := range in {
+			if n := countEqual(out, v); n != numberOfChannels {
+				t.Errorf("expected %d of value %d got %d", numberOfChannels, v, n)
+			}
+		}
+	})
+}
+
+func countEqual[T comparable](slice []T, val T) int {
+	n := 0
+	for _, v := range slice {
+		if v == val {
+			n++
+		}
+	}
+	return n
+}
+
+func countEqualFunc[T any](slice []T, val T, equal func(T, T) bool) int {
+	n := 0
+	for _, v := range slice {
+		if equal(v, val) {
+			n++
+		}
+	}
+	return n
 }
